482e396a469462304232639266b55f4731af974a
surya <surya.natuva@improve10x.com>
2023-02-01 11:18:12 +0530

    new

---
+++ app/src/main/java/com/improve10x/movieseries/MovieSeriesActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.movieseries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieSeriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_series);
+    }
+}
